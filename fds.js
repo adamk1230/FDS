b8b809851958682c30524644c9352bccaeb2f9b6
Adam Kwan <adamk1230@gmail.com>
2018-01-29 16:06:02 -0500

    initial commit, commented instructions

---
+++ fds.js
@@ -0,0 +1,21 @@
+//   Create a Github repository and commit to it often (it is helpful to see 
+//   how you break down the work versus one commit at the end) 
+//   2) While you are writing the application, create tests that verify your 
+//   answer solves the solution
+  
+//   *Factorial Digit Sum*
+  
+//   *Goal:* 
+//   Create an application to find the sum of the digits from factorial(100)
+  
+//   *Structure:* 
+//   Our factorial function should exist in the form factorial(n) = n x (n - 1) 
+//   x (n - 2) x ... x 3 x 2 x 1
+  
+//   *An example of factorial(10)* 
+//   factorial(10) = 10 x 9 x 8 x ... x 3 x 2 x 1 = 3628800
+  
+//   *Sum of the factorial output digits* 
+//   Calculate the sum of the output digits so that 3 + 6 + 2 + 8 + 8 + 0 + 0 = 
+//   27
+
